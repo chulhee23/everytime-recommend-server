61b3bcbb86c34f05c99f43f85f8002854f2d5ec9
chulhee23 <chulhee23@naver.com>
2019-10-28 00:39:46 +0900

    bootstrap scss setting

--- project_everyReco/settings.py
+++ project_everyReco/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
+    'main',
+    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main', 'static')
+]
+STATICFILES_FINDERS = [
+    'compressor.finders.CompressorFinder'
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
