544f11e49472c119b08b87c908d3b3f1ac1f4738
HyuuunjuKim <gus7wn@gmail.com>
2019-10-31 17:29:28 +0900

    add result.html, button

--- main/views.py
+++ main/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request) :
-
     return render(request, 'home.html')
 
 def temp(request):
     return render(request, 'temp.html')
 
-def result(request, type):
-    
+def result(request):
     return render(request, 'result.html')
