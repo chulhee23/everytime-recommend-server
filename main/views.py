98a571e497f624aad6d555a3f81e104d18e73560
HyuuunjuKim <gus7wn@gmail.com>
2019-11-01 18:27:00 +0900

    검색 flow 작업

--- main/views.py
+++ main/views.py
@@ -8,4 +8,12 @@ def temp(request):
     return render(request, 'temp.html')
 
 def result(request):
-    return render(request, 'result.html')
+    lecture = request.GET.get('lecture') #강의명 검색 value
+    professor = request.GET.get('professor') #교수명 검색 value
+
+    if lecture != '' and professor != '' : #둘 다 검색시
+        return render(request, 'resultBoth.html', {'lecture': lecture, 'professor':professor})
+    elif lecture != '' and professor == '' : #강의명만 검색시
+        return render(request, 'resultLecture.html', {'lecture': lecture})
+    elif lecture == '' and professor != '' : #교수명만 검색시
+        return render(request, 'resultProf.html', {'professor':professor})
