832c29b9a1680c20cdb5c4eb7210814dacba35d7
Aqudi <gjdigj@naver.com>
2019-11-15 20:55:17 +0900

    update initial_data

--- utils/initial_data_generator.py
+++ utils/initial_data_generator.py
@@ -5,21 +5,20 @@ from pathlib import Path
 
 
 def dataFormater(fields):
-  data_list = list()
-  for i, field in enumerate(fields):
-    data_list.append({
-      "model": "main.lecture",
-      "pk":i+1,
-      "fields":field
-    })
-  return data_list
+    data_list = list()
+    for i, field in enumerate(fields):
+        data_list.append({
+            "model": "main.lecture",
+            "pk": i + 1,
+            "fields": field
+        })
+    return data_list
 
 
-cwd = Path('..')
-app_name = "main"
-dir_name = "module"
-file_name = "lecture106.csv"
-full_path = cwd/app_name/dir_name/file_name
+cwd = Path('.')
+dir_name = "data"
+file_name = "lecture.csv"
+full_path = cwd / dir_name / file_name
 
 data = pd.read_csv(full_path, encoding="utf-8")
 data.reset_index()
@@ -31,8 +30,7 @@ data = data.to_dict(orient="records")
 data = dataFormater(data)
 # json_data = json.dumps(data)
 
-write_file_path = cwd/Path("initial_data.json")
+write_file_path = cwd / Path("data/initial_data.json")
 
 with open(write_file_path, 'w', encoding='UTF-8') as f:
     json.dump(data, f, ensure_ascii=False)
-
